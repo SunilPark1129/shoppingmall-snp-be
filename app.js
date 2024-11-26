5cfa253fe9a73612c45772d3d9727f8778a781e4
Sunil Park <sunilpark1129@gmail.com>
2024-11-26 14:13:52 -0500

    edited - local port

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ mongoose
   .then(() => console.log("mongoose conntected"))
   .catch((err) => console.log("DB conntection fail", err));
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("server on");
 });
