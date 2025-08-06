40c0b6e84e36cb7cd3cc31e898a26a27327acdc3
Sunil Park <sunilpark1129@gmail.com>
2025-08-06 19:07:31 -0400

    fix: resolve error

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api", indexRouter);
-app.get("/", () => {
+app.get("/", (req, res) => {
   res.send("the server is listening...");
 });
 
